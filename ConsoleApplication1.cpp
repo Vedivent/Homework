d3c1cf6ee62d08ca4a3f2461b98647470dff625f
Stanislav <59729557+Vedivent@users.noreply.github.com>
2022-04-06 12:03:58 +0300

    YazProg.2022

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,345 @@
+﻿//Shaudylin s v ibo-zi-20
+
+#include <iostream>
+#include <cmath>
+#include <math.h>
+#include <thread>
+#include <chrono>
+#include <limits>
+#include <iomanip>
+
+using namespace std;
+
+void _11e() {   //write an arithmetic expression in an algorithmic language and specify the order of operations.
+    double a;
+    cout << "Enter side of square" << endl;
+    cin >> a;
+    if (a == 0) {
+        cout << "The square is null";
+    }
+    else if (a > 0) {
+        cout << "The perimeter is " << setprecision(10) << a * 4;
+    }
+    else {
+        cout << "Sorry, only POSITIVE numbers" << endl;
+    }
+    cout << endl << endl;
+}
+
+void _11m() {   //write an arithmetic expression in an algorithmic language and specify the order of operations.
+    const double PI = 3.1415926535;
+    double r;
+    cout << "Enter the radius" << endl;
+    cin >> r;
+    if (r == 0) {
+        cout << "The circle is null";
+    }
+    else if (r > 0) {
+        cout << "The perimeter is " << setprecision(10) << (2 * PI * r);
+    }
+    else {
+        cout << "Sorry, only POSITIVE numbers" << endl;
+    }
+    cout << endl << endl;
+}
+
+void _12e() { //Programming of linear algorithms - easy
+    double a, b, c;
+    cout << "Enter two numbers" << endl;
+    cin >> a >> b;
+    c = static_cast<double>((pow(a, 3) + pow(b, 3)) / 2);
+    cout << "Arithmetic mean of cubes of numbers is " << c << setw(5) << endl;
+    cout << "The geometric mean of the modules of numbers is " << (sqrt(abs(a * b))) << endl;
+    cout << endl << endl;
+}
+
+void _12m() { //Programming of linear algorithms - middle
+    cout << "Enter one of two ways: 1 or 2" << endl;
+
+    unsigned short f = 0;
+    cin >> f;
+    switch (f) {
+    case 1: {
+        cout << "B is 4.2. Enter number T" << endl;
+        const double b = 4.2;
+        double t, x, y;
+        cin >> t;
+        double a = t + pow(b, 3);
+        x = pow(t, 2) * (sqrt(a + b));
+        y = pow(log(abs(x + pow(a, 2))), 5);
+        cout << "a is " << a << setw(8) << endl;
+        cout << "x is " << x << setw(8) << endl;
+        cout << "y is " << y << setw(8) << endl;
+        break; }
+    case 2: {
+        cout << "T is 3. Enter number B" << endl;
+        const double T = 3;
+        double B, X, Y;
+        cin >> B;
+        double A = T + pow(B, 3);
+        X = pow(T, 2) * (sqrt(A + B));
+        Y = pow(log(abs(X + pow(A, 2))), 5);;
+        cout << "a is " << A << setw(8) << endl;
+        cout << "x is " << X << setw(8) << endl;
+        cout << "y is " << Y << setw(8) << endl;
+        break; }
+    default: {
+        cout << "You chose wrong!" << endl;
+        break; }
+    }
+}
+
+void _21e() { //Programming branching algorithms. logical expressions - easy
+    cout << "Write a logical expression describing the scope of the definition and the scope of the function change" << endl;
+    cout << "y = 1 / x^2" << endl;
+    double x = 0;
+    double a = 0;
+    cin >> x;
+    if (x == 1) {
+        cout << "y = 1" << endl;
+    }
+    else if ((x > 1) || (x < 0)) {
+        a = (1 / (pow(x, 2)));
+        cout << a << setw(20) << endl;
+    }
+    else {
+        cout << "X is incorrect" << endl;
+    }
+    cout << endl << endl;
+}
+
+void _21m() { //Programming branching algorithms. logical expressions - middle
+    cout << "Write a logical expression defining that of the four numbers A, B, C, D one is divisible by 5 and the other is even. Enter them." << endl;
+    int A, B, C, D = 0;
+    cin >> A >> B >> C >> D;
+    if ((A % 2 == 0 || B % 2 == 0 || C % 2 == 0 || D % 2 == 0) &&
+        (A % 5 == 0 || B % 5 == 0 || C % 5 == 0 || D % 5 == 0) &&
+        (A % 10 != 0 && B % 10 != 0 && C % 10 != 0 && D % 10 != 0))
+    {
+        cout << "The numbers fit" << endl;
+    }
+    else {
+        cout << "The numbers don't fit" << endl;
+    }
+    cout << endl << endl;
+}
+
+void _22e() { //Given a natural number. Determine whether it is even or ends with the digit 7.
+    int a = 0;
+    cout << "Enter natural number:  " << endl;
+    cin >> a;
+    if ((a < 0)) {
+        cout << "incorrect number " << a;
+    }
+    else if ((a % 2 == 0) || (a % 10 == 7)) {
+        cout << "The number " << a << " is even OR it ends with 7"  << endl;
+    }
+    else {
+        cout << "Nope, the number doesn't fit " << endl;
+    }
+}
+
+void _22m() { //Enter three numbers from the keyboard.Display the largest of them on the screen
+    double a, b, c = 0;
+    cout << "Enter three numbers: " << endl;
+    cin >> a >> b >> c;
+    if ((a > b) && (a > c)) {
+        cout << "The largest number is a " << a << endl;
+    }
+    if ((b > a) && (b > c)) {
+        cout << "The largest number is b " << b << endl;
+    }
+    if ((c > a) && (c > b)) {
+        cout << "The largest number is c " << c << endl;
+    }
+    else {
+        cout << "Sorry, I can't count :c " << endl;
+    }
+}
+
+void _23e() { //write a program for calculating the function for all variants of variables.
+    short a = 0;
+    cout << "We can get the resistance with the help of two 6kOhm resistors..." << endl;
+    cout << "Select the option number: 1 / 2 / 3" << endl;
+    cin >> a;
+    switch (a) {
+        case 1: { // R1=6, R2=10, R3=2
+            cout << "R1=6, R2=10, R3=2" << endl;
+            cout << "BOO";
+
+        }
+        case 2: {// R1=3, R2=5, R3=7
+            cout << "R1=3, R2=5, R3=7" << endl;
+
+        }
+        case 3: {// R1=4, R2=12, R3=8
+            cout << "R1=4, R2=12, R3=8" << endl;
+
+        }
+        default: {
+        cout << "I N V A L I D  O P T I O N!" << endl;
+        }
+    }
+
+}
+
+void _23m() { //Write a program for calculating the function y(x) for all different variants of input parameters. Enter x from the keyboard, output the function value for all options.
+
+}
+
+void _31e() { //Given natural numbers from 10 to N. Find the smallest digit of an integer decimal number.
+    int x = 0;
+
+}
+
+void _31m() { //draw up an algorithm diagram and write a program for calculating the sum (product) of a finite series. When developing a program, provide for the omission of terms (multipliers) equal to zero or infinity.
+
+}
+
+void _32e() { //Find the sum of the squares of even numbers in the interval given by the values of the variables m and n.
+    int sum = 0, M = 0, N = 0, q = 0;
+    cout << "Enter the interval M and N" << endl;
+    cin >> M >> N;
+    if (M > N)
+        swap(N, M);
+    if (M % 2 != 0)
+        M++;
+    for (int x = M; x <= N; x += 2) {
+        q = pow(x, 2);
+        sum += q;
+    }
+    cout << "Sum of the squares of even numbers in the interval is " << sum << endl;
+}
+
+void _32m() { //Natural numbers from 0 to n (n<99) and the number m are given. And find the square of the first number greater than m .
+    int n = 0, m = 0, q = 0;
+    cout << "Enter N (0<n<99) and M" << endl;
+    cin >> n >> m;
+    if ((n < 0) || (n > 99)) {
+        cout << "Wrong N!" << endl;
+    }
+    else {
+        for (int x = 0; x <= n; x++) {
+            q = pow(x, 2);
+            if (q >= m) {
+                cout << q << " is larger than M = " << m << endl;
+                break;
+            }
+            else if ((q < m)&&(x == n)) {
+                cout << "There's no numbers larger than M = " << m << endl;
+                break;
+            }
+        }   
+    }
+}
+
+int main() {
+    unsigned short n = 1;
+    while ((n != 0) && (n <= 14) && (n >= 0)) {
+        cout << "V A R I A N T      3 0.    To exit enter 0." << endl << endl;
+        cout << "! ! ! ENTERING ANOTHER SYMBOLS CAUSES ERRORS AND CRASHING ! ! !" << endl << endl;
+        cout << "Enter the number of exercise:" << endl;
+        for (unsigned short i = 1; i <= 14; i++) {
+            cout << i << " ";
+        }
+        cout << endl << endl << "Enter here:    ";
+        cin >> n;
+        switch (n) {
+            case 0: {
+                cout << "Thanks for checking!" << endl << endl << endl;
+                break;}
+            case 1: {
+                _11e();
+                system("pause");
+                system("cls");
+                break;}
+            case 2: {
+                _11m();
+                system("pause");
+                system("cls");
+                break;}
+            case 3: {
+                _12e();
+                system("pause");
+                system("cls");
+                break;}
+            case 4: {
+                _12m();
+                system("pause");
+                system("cls");
+                break;}
+            case 5: {
+                _21e();
+                system("pause");
+                system("cls");
+                break;}
+            case 6: {
+                _21m();
+                system("pause");
+                system("cls");
+                break;}
+            case 7: {
+                _22e();
+                system("pause");
+                system("cls");
+                break;}
+            case 8: {
+                _22m();
+                system("pause");
+                system("cls");
+                break;}
+            case 9: {
+                _23e();
+                system("pause");
+                system("cls");
+                break;}
+            case 10: {
+                _23m();
+                system("pause");
+                system("cls");
+                break;}
+            case 11: {
+                _31e();
+                system("pause");
+                system("cls");
+                break; }
+            case 12: {
+                _31m();
+                system("pause");
+                system("cls");
+                break; }
+            case 13: {
+                _32e();
+                system("pause");
+                system("cls");
+                break; }
+            case 14: {
+                _32m();
+                system("pause");
+                system("cls");
+                break; }
+            default: {
+                cout << "I N V A L I D  N U M B E R!" << endl;
+                system("pause");
+                break;}
+        }
+    }
+}
+
+/* patterns
+
+
+case : {
+            _();
+            system("pause");
+            system("cls");
+            break;}
+            
+
+
+void _23m() { //
+
+}
+
+
+*/
