05ddf14c0da6812fc932d4db6e1338ed979a98cb
Stanislav <59729557+Vedivent@users.noreply.github.com>
2022-12-04 23:08:11 +0300

    Create prakt7.cpp

---
+++ 2022-2023/prakt7.cpp
@@ -0,0 +1,52 @@
+// shaydulin
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+class Transport
+{
+public:
+    Transport();
+    ~Transport();
+
+protected:
+    double GetCost() { return cost; }
+    void SetCost(double other) { cost = other; }
+    double cost;
+    std::string GetDestiny() { return destiny; }
+    void SetDestiny(std::string other) { destiny = other; }
+    std::string destiny;
+};
+
+class Transport1 : Transport
+{
+public:
+    Transport1();
+    ~Transport1();
+    char GetField1() { return field1; }
+    char SetField1(char other) { field1 = other; }
+    char GetField2() { return field2; }
+    char SetField2(char other) { field2 = other; }
+
+private:
+    char field1;
+    char field2;
+};
+
+class Transport2 : Transport
+{
+public:
+    Transport2();
+    ~Transport2();
+    char GetField1() { return field1; }
+    char SetField1(char other) { field1 = other; }
+    char GetField2() { return field2; }
+    char SetField2(char other) { field2 = other; }
+
+private:
+    char field1;
+    char field2;
+};
